25b4037b1c98e27a60f74183ae8d3321d356a015
Dani Pérez <dperezl@vectorsf.com>
2015-09-29 17:45:22 +0200

    7 commit

--- STANDARD/assets/js/controllers/dashboardCtrl.js
+++ STANDARD/assets/js/controllers/dashboardCtrl.js
@@ -31,7 +31,7 @@ app.controller('VisitsCtrl', [ "$scope", "$http", "$timeout", function ($scope, 
             //$scope.botonA = true;
         });
 
-        },5000);
+        },2000);
 
         $timeout( function(){
 
@@ -55,92 +55,102 @@ app.controller('VisitsCtrl', [ "$scope", "$http", "$timeout", function ($scope, 
                 $scope.mensaje = true;
             }
 
-            $scope.data = {};
+/*            $scope.data = {};
 
             $scope.data.labels = $scope.labels;
             $scope.data.datasets[0].data = $scope.dataRec[0];
             $scope.data.datasets[1].data = $scope.dataRec[1];
-            $scope.data.datasets[2].data = $scope.dataRec[2];
+            $scope.data.datasets[2].data = $scope.dataRec[2];*/
             
-        },8000);
-   /* $scope.data = {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        datasets: [
-          {
-              label: 'My First dataset',
-              fillColor: 'rgba(220,220,220,0.2)',
-              strokeColor: 'rgba(220,220,220,1)',
-              pointColor: 'rgba(220,220,220,1)',
-              pointStrokeColor: '#fff',
-              pointHighlightFill: '#fff',
-              pointHighlightStroke: 'rgba(220,220,220,1)',
-              data: [65, 59, 80, 81, 56, 55, 40, 84, 64, 120, 132, 87]
-          },
-          {
-              label: 'My Second dataset',
-              fillColor: 'rgba(151,187,205,0.2)',
-              strokeColor: 'rgba(151,187,205,1)',
-              pointColor: 'rgba(151,187,205,1)',
-              pointStrokeColor: '#fff',
-              pointHighlightFill: '#fff',
-              pointHighlightStroke: 'rgba(151,187,205,1)',
-              data: [28, 48, 40, 19, 86, 27, 90, 102, 123, 145, 60, 161]
-          }
-        ]
-    };*/
+            $scope.data = {
+                labels: $scope.labels,
+                datasets: [
+                  {
+                      label: $scope.series[0],
+                      fillColor: 'rgba(220,220,220,0.2)',
+                      strokeColor: 'rgba(220,220,220,1)',
+                      pointColor: 'rgba(220,220,220,1)',
+                      pointStrokeColor: '#fff',
+                      pointHighlightFill: '#fff',
+                      pointHighlightStroke: 'rgba(220,220,220,1)',
+                      data: $scope.dataRec[0]
+                  },
+                  {
+                      label: $scope.series[1],
+                      fillColor: 'rgba(151,187,205,0.2)',
+                      strokeColor: 'rgba(151,187,205,1)',
+                      pointColor: 'rgba(151,187,205,1)',
+                      pointStrokeColor: '#fff',
+                      pointHighlightFill: '#fff',
+                      pointHighlightStroke: 'rgba(151,187,205,1)',
+                      data:  $scope.dataRec[1]
+                  },
+                  {
+                      label: $scope.series[2],
+                      fillColor: 'rgba(0,187,205,0.2)',
+                      strokeColor: 'rgba(0,187,205,1)',
+                      pointColor: 'rgba(0,187,205,1)',
+                      pointStrokeColor: '#fff',
+                      pointHighlightFill: '#fff',
+                      pointHighlightStroke: 'rgba(151,187,205,1)',
+                      data:  $scope.dataRec[2]
+                  }
+                ]
+            };
 
-    $scope.options = {
+            $scope.options = {
 
-        maintainAspectRatio: false,
+                maintainAspectRatio: false,
 
-        // Sets the chart to be responsive
-        responsive: true,
+                // Sets the chart to be responsive
+                responsive: true,
 
-        ///Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines: true,
+                ///Boolean - Whether grid lines are shown across the chart
+                scaleShowGridLines: true,
 
-        //String - Colour of the grid lines
-        scaleGridLineColor: 'rgba(0,0,0,.05)',
+                //String - Colour of the grid lines
+                scaleGridLineColor: 'rgba(0,0,0,.05)',
 
-        //Number - Width of the grid lines
-        scaleGridLineWidth: 1,
+                //Number - Width of the grid lines
+                scaleGridLineWidth: 1,
 
-        //Boolean - Whether the line is curved between points
-        bezierCurve: false,
+                //Boolean - Whether the line is curved between points
+                bezierCurve: false,
 
-        //Number - Tension of the bezier curve between points
-        bezierCurveTension: 0.4,
+                //Number - Tension of the bezier curve between points
+                bezierCurveTension: 0.4,
 
-        //Boolean - Whether to show a dot for each point
-        pointDot: true,
+                //Boolean - Whether to show a dot for each point
+                pointDot: true,
 
-        //Number - Radius of each point dot in pixels
-        pointDotRadius: 4,
+                //Number - Radius of each point dot in pixels
+                pointDotRadius: 4,
 
-        //Number - Pixel width of point dot stroke
-        pointDotStrokeWidth: 1,
+                //Number - Pixel width of point dot stroke
+                pointDotStrokeWidth: 1,
 
-        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-        pointHitDetectionRadius: 20,
+                //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+                pointHitDetectionRadius: 20,
 
-        //Boolean - Whether to show a stroke for datasets
-        datasetStroke: true,
+                //Boolean - Whether to show a stroke for datasets
+                datasetStroke: true,
 
-        //Number - Pixel width of dataset stroke
-        datasetStrokeWidth: 2,
+                //Number - Pixel width of dataset stroke
+                datasetStrokeWidth: 2,
 
-        //Boolean - Whether to fill the dataset with a colour
-        datasetFill: true,
+                //Boolean - Whether to fill the dataset with a colour
+                datasetFill: true,
 
-        // Function - on animation progress
-        onAnimationProgress: function () { },
+                // Function - on animation progress
+                onAnimationProgress: function () { },
 
-        // Function - on animation complete
-        onAnimationComplete: function () { },
+                // Function - on animation complete
+                onAnimationComplete: function () { },
 
-        //String - A legend template
-        legendTemplate: '<ul class="tc-chart-js-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
-    };
+                //String - A legend template
+                legendTemplate: '<ul class="tc-chart-js-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
+            };
+        },3000);
 
 }]);
 app.controller('SalesCtrl', ["$scope", function ($scope) {
